68cbb7db640e6fee2ccf83ab3535aae1b7c15f76
ezra <juanzhe2@gmail.com>
2026-08-20 16:43:12 +0800

    feat: restructure routes and enhance AI interaction handling in Hono app

--- src/index.ts
+++ src/index.ts
@@ -9,34 +9,41 @@ type Bindings = {
 
 const app = new Hono<{ Bindings: Bindings }>();
 
-app.post("/telegram/webhook", async (c) => {
+app.get("/", (c) => {
+  return c.text("Hello, World!");
+});
+
+app.get("/ai", async (c) => {
   const ai = new GoogleGenAI({
     apiKey: c.env.AI_API_KEY,
   });
 
+  const interaction = await ai.interactions.create({
+    model: "gemini-3.5-flash-lite",
+    input: `
+You are a café ordering assistant.
+
+Menu:
+- Chicken Rice — RM8
+- Nasi Lemak — RM7
+- Iced Coffee — RM4
+- Teh Ais — RM3
+
+Customer:
+"I want two chicken rice and one iced coffee."
+  `,
+  });
+  return c.text(
+    interaction.output_text ? interaction.output_text : "No output available",
+  );
+});
+
+app.post("/telegram/webhook", async (c) => {
   const update = await c.req.json();
 
   console.log("Received update:", update);
 
   return c.json({ ok: true });
-  //   const interaction = await ai.interactions.create({
-  //     model: "gemini-3.5-flash-lite",
-  //     input: `
-  // You are a café ordering assistant.
-
-  // Menu:
-  // - Chicken Rice — RM8
-  // - Nasi Lemak — RM7
-  // - Iced Coffee — RM4
-  // - Teh Ais — RM3
-
-  // Customer:
-  // "I want two chicken rice and one iced coffee."
-  //   `,
-  //   });
-  //   return c.text(
-  //     interaction.output_text ? interaction.output_text : "No output available",
-  //   );
 });
 
 export default app;
